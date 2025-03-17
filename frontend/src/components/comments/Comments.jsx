d3ac211ba8501b1dcdb7529642a926a3277ce4f1
mahnoor3650 <mahnoorkhalidhashmi138@gmail.com>
2025-03-18 00:24:46 +0500

    1:28

--- frontend/src/components/comments/Comments.jsx
+++ frontend/src/components/comments/Comments.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
-import "./comments.css"
+import React from "react";
+import "./comments.css";
+import Image from "../image/Image";
 const Comments = () => {
   return (
-    <div>
-      
+    <div className="comments">
+      <div className="commentList">
+        <span className="commentCount">5 Comments</span>
+        <div className="comment">
+          <Image path="/general/noAvatar.png" alt="" />
+          <div className="commentContent">
+            <span className="commentUsername">John Doe</span>
+            <p className="commentText">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+              quos.
+            </p>
+            <span className="commentTime">1h</span>
+          </div>
+        </div>
+        <div className="comment">
+          <Image path="/general/noAvatar.png" alt="" />
+          <div className="commentContent">
+            <span className="commentUsername">John Doe</span>
+            <p className="commentText">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+              quos.
+            </p>
+            <span className="commentTime">1h</span>
+          </div>
+        </div>
+        <div className="comment">
+          <Image path="/general/noAvatar.png" alt="" />
+          <div className="commentContent">
+            <span className="commentUsername">John Doe</span>
+            <p className="commentText">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+              quos.
+            </p>
+            <span className="commentTime">1h</span>
+          </div>
+        </div>
+        <div className="comment">
+          <Image path="/general/noAvatar.png" alt="" />
+          <div className="commentContent">
+            <span className="commentUsername">John Doe</span>
+            <p className="commentText">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
+              quos.
+            </p>
+            <span className="commentTime">1h</span>
+          </div>
+        </div>
+      </div>
+      <form className="commentForm">
+        <input
+          type="text"
+          className="commentInput"
+          placeholder="Add a comment"
+        />
+        <div className="emoji">
+          <div>😀</div>
+        </div>
+        <button className="commentButton">Send</button>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Comments
+export default Comments;
